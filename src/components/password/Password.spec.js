a2ceedbffdf3b57358170b01605eb3412384ca14
gabrieladelfino <gabriela.delfino@easynvest.com.br>
2019-01-10 10:52:26 -0200

    Atualiza dependencias

--- src/components/password/Password.spec.js
+++ src/components/password/Password.spec.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import Password from './Password'
 import { render, cleanup } from 'react-testing-library'
+import 'jest-dom/extend-expect'
 
 afterEach(cleanup)
 
 describe('Password', () => {
   it('should mount component', () => {
-    const { getByText } = render(
-      <Password />
-    )
-
+    const { getByText } = render(<Password />)
     expect(getByText('Ter no mínimo 6 caracteres')).toBeInTheDocument()
   })
 }) 
